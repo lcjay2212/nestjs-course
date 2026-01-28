a6a4967695999a2ecbdec6e04aa37c562f657633
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2026-01-28 09:52:38 +0800

    Adds task creation endpoint
    Implements the functionality for creating new tasks.
    
    Introduces a POST endpoint in the TasksController, allowing
    users to create tasks by providing a title and description.
    
    The new TasksService handles the creation of tasks, assigning
    a unique ID and setting the default status to OPEN.
    
    Also configures VSCode to format on save with Prettier.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,18 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: randomUUID(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
